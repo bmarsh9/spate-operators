90a9dd9360f8f4cfe6ca2588f150ad1fb41982aa
bmarsh9 <bmarshall735@gmail.com>
2021-12-13 21:22:13 -0500

    Update network_connection.py

--- operators/network_connection.py
+++ operators/network_connection.py
@@ -1,7 +1,7 @@
 def code(input, **kwargs):
     '''Place your custom code below.
     Must be indented under this function.'''
-    url = locker(kwargs["config"],"default","url_host") ##input:type=text:name=url_host:label=Enter the hostname for the URL connection:placeholder=e.g http://test.com
+    url = locker(kwargs["config"],"default","url_host") ##input:type=text:name=url_host:label=Enter the hostname for the URL connection:placeholder=e.g 'http://test.com'
     r = request.get(url)
     if r.ok:
         return r.json()
