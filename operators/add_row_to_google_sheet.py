64f9bb7d319b7a0cd7027167c62b15c26727eaec
bmarsh9 <bmarshall735@gmail.com>
2021-12-14 11:47:24 -0500

    Update add_row_to_google_sheet.py

--- operators/add_row_to_google_sheet.py
+++ operators/add_row_to_google_sheet.py
@@ -2,6 +2,8 @@ def code(input, **kwargs):
     '''Place your custom code below.
     Must be indented under this function.'''
 
+    secret_dict = locker(kwargs["config"],"default","004_secretdict") ##input:type=text:name=004_secretdict:label=Oauth credentials :placeholder=e.g. {}
+
     # https://github.com/nithinmurali/pygsheets
     SCOPES = ('https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive')
     service_account_info = json.loads(secret_dict)
