9776f69863f8f8b1ef8173838c1243408dbbe693
bmarsh9 <bmarshall735@gmail.com>
2021-12-14 14:41:30 -0500

    Update add_row_to_google_sheet.py

--- operators/add_row_to_google_sheet.py
+++ operators/add_row_to_google_sheet.py
@@ -3,8 +3,9 @@ def code(input, **kwargs):
     Must be indented under this function.'''
     # https://github.com/nithinmurali/pygsheets
     secret_dict = locker(kwargs["config"],"default","004_secretdict") ##input:type=text:name=004_secretdict:label=Oauth credentials:placeholder=e.g. {}
-    spreadsheet_key = locker(kwargs["config"],"default","004_sheetkey") ##input:type=text:name=004_sheetkey:label=Enter the key of the Google spreadsheet - See here pygsheets.readthedocs.io/en/latest/authorization.html
-    sheet_name = locker(kwargs["config"],"default","004_sheetname") ##input:type=text:name=004_sheetname:label=Enter the name of the Google sheet tab
+    spreadsheet_key = locker(kwargs["config"],"default","004_sheetkey") ##input:type=text:name=004_sheetkey:placeholder=Key is found in the gsheet url:label=Enter the key of the Google spreadsheet, case sensitive - See here pygsheets.readthedocs.io/en/latest/authorization.html
+    sheet_name = locker(kwargs["config"],"default","004_sheetname") ##input:type=text:name=004_sheetname:label=Enter the name of the Google sheet tab, case sensitive
+    values = locker(kwargs["config"],"default","004_values") ##input:type=text:name=004_values:label=Enter the values to append to the google sheet, comma separated
 
     try:
         service_account_info = json.loads(secret_dict)
@@ -22,7 +23,7 @@ def code(input, **kwargs):
     sheet = gc.open_by_key(spreadsheet_key)
     wks = sheet.worksheet_by_title(sheet_name)
     cells = wks.get_all_values(include_tailing_empty_rows=False, include_tailing_empty=False, returnas='matrix')
-    wks.insert_rows(row=len(cells), number=1, values=[1,2])
+    wks.insert_rows(row=len(cells), number=1, values=values.split(","))
     logging.info("added row to the spreadsheet:{}".format(sheet_name))
     
     '''Default return is True. If you want to return something else, do so above.
