552d750d987427a8bc36f7bf4c70448ea2a3295a
bmarsh9 <bmarshall735@gmail.com>
2021-12-13 21:52:53 -0500

    Update network_connection.py

--- operators/network_connection.py
+++ operators/network_connection.py
@@ -3,7 +3,7 @@ def code(input, **kwargs):
     Must be indented under this function.'''
     url = locker(kwargs["config"],"default","url_host") ##input:type=text:name=url_host:label=Enter the hostname for the URL connection:placeholder=Enter the host with http
     verify = locker(kwargs["config"],"default","verify") ##input:type=checkbox:name=verify:label=Verify TLS connection
-    headers = locker(kwargs["config"],"default","headers") or {} ##input:type=text:name=headers:label=Set TLS headers:placeholder=Dictionary of headers for the request    
+    headers = locker(kwargs["config"],"default","headers") or {} ##input:type=text:name=headers:label=Set headers:placeholder=Dictionary of headers for the request    
     r = requests.get(url,verify=verify,headers=headers)
     if r.ok:
         return r.json()
