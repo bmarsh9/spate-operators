21340c72b7905042e9aca266cff8fc3df0443e40
bmarsh9 <bmarshall735@gmail.com>
2021-12-14 11:19:16 -0500

    Update send_text_message.py

--- operators/send_text_message.py
+++ operators/send_text_message.py
@@ -5,14 +5,14 @@ def code(input, **kwargs):
     account_sid = locker(kwargs["config"],"default","003_accountsid") ##input:type=text:name=003_accountsid:label=Enter your account SID - found at twilio.com/console
     auth_token = locker(kwargs["config"],"default","003_authtoken") ##input:type=text:name=003_authtoken:label=Enter authentication token - found at twilio.com/console
     body = locker(kwargs["config"],"default","003_body") ##input:type=text:name=003_body:label=Enter the body of the text message
-    from = locker(kwargs["config"],"default","003_from") ##input:type=text:name=003_from:label=Enter source phone number
-    to = locker(kwargs["config"],"default","003_to") ##input:type=text:name=003_to:label=What phone number should we send the message to?
+    _from = locker(kwargs["config"],"default","003_from") ##input:type=text:name=003_from:label=Enter source phone number
+    _to = locker(kwargs["config"],"default","003_to") ##input:type=text:name=003_to:label=What phone number should we send the message to?
 
     if not account_sid or not auth_token:
         logging.error("<account_sid> and <auth_token> is required")
         return False
     
-    if not from or not to:
+    if not _from or not _to:
         logging.error("<from> and <to> is required")
         return False
     client = Client(account_sid, auth_token)
@@ -20,11 +20,14 @@ def code(input, **kwargs):
     message = client.messages \
         .create(
             body=body,
-            from_="+1{}".format(from),
-            to="+1{}".format(to)
+            from_="+1{}".format(_from),
+            to="+1{}".format(_to)
         )
-    logging.info("successfully sent the text message to {}".format(to))
-
+    if message.sid.get("status") == "sent:
+        logging.info("successfully sent the text message to {}".format(to))
+    else:
+        logging.warning("unable to send the text message. check the raw debug message)
+    logging.debug("debug message from twilio:{}".format(str(message.sid)))
     '''Default return is True. If you want to return something else, do so above.
     If the return is False, the workflow will NOT proceed.'''
     return input
