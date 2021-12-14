7bba911903a82dd406a3386ecbb86b2968804107
bmarsh9 <bmarshall735@gmail.com>
2021-12-13 23:02:31 -0500

    Update network_connection.py

--- operators/network_connection.py
+++ operators/network_connection.py
@@ -1,13 +1,13 @@
 def code(input, **kwargs):
     '''Place your custom code below.
     Must be indented under this function.'''
-    url = locker(kwargs["config"],"default","url_host") ##input:type=text:name=url_host:label=Enter the hostname for the URL connection:placeholder=Enter the host with http
-    verify = locker(kwargs["config"],"default","verify") ##input:type=checkbox:name=verify:label=Verify TLS connection
-    headers = locker(kwargs["config"],"default","headers") or {} ##input:type=text:name=headers:label=Set headers:placeholder=Dictionary of headers for the request
+    url = locker(kwargs["config"],"default","001_url_host") ##input:type=text:name=001_url_host:label=Enter the hostname for the URL connection:placeholder=Enter the host with http
+    verify = locker(kwargs["config"],"default","001_verify") ##input:type=checkbox:name=001_verify:label=Verify TLS connection
+    headers = locker(kwargs["config"],"default","001_headers") ##input:type=text:name=001_headers:label=Set headers:placeholder=Dictionary of headers for the request
     
     # format headers for the request
     try:
-        headers = json.loads(headers)
+        headers = json.loads(headers) 
     except:
         logging.warning("Invalid header format. Must be type <dict>")
         headers = {}
