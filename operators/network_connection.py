c7a8a226bca42905fff3821cdfe6d653de6ac3e2
bmarsh9 <bmarshall735@gmail.com>
2021-12-13 21:04:55 -0500

    Update network_connection.py

--- operators/network_connection.py
+++ operators/network_connection.py
@@ -1,2 +1,8 @@
 def code(input, **kwargs):
+    '''Place your custom code below.
+    Must be indented under this function.'''
+
+
+    '''Default return is True. If you want to return something else, do so above.
+    If the return is False, the workflow will NOT proceed.'''
     return input
