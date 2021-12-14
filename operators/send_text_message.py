b797b5d79816b08d452f0a8eae143e92f73f0352
bmarsh9 <bmarshall735@gmail.com>
2021-12-14 11:13:50 -0500

    Update send_text_message.py

--- operators/send_text_message.py
+++ operators/send_text_message.py
@@ -4,18 +4,25 @@ def code(input, **kwargs):
 
     account_sid = locker(kwargs["config"],"default","003_accountsid") ##input:type=text:name=003_accountsid:label=Enter your account SID - found at twilio.com/console
     auth_token = locker(kwargs["config"],"default","003_authtoken") ##input:type=text:name=003_authtoken:label=Enter authentication token - found at twilio.com/console
+    body = locker(kwargs["config"],"default","003_body") ##input:type=text:name=003_body:label=Enter the body of the text message
+    from = locker(kwargs["config"],"default","003_from") ##input:type=text:name=003_from:label=Enter source phone number
+    to = locker(kwargs["config"],"default","003_to") ##input:type=text:name=003_to:label=What phone number should we send the message to?
 
     if not account_sid or not auth_token:
         logging.error("<account_sid> and <auth_token> is required")
         return False
+    
+    if not from or not to:
+        logging.error("<from> and <to> is required")
+        return False
     client = Client(account_sid, auth_token)
 
     message = client.messages \
-                    .create(
-                         body=body,
-                         from_="+1{}".format(from),
-                         to="+1{}".format(to)
-                     )
+        .create(
+            body=body,
+            from_="+1{}".format(from),
+            to="+1{}".format(to)
+        )
     logging.info("successfully sent the text message to {}".format(to))
 
     '''Default return is True. If you want to return something else, do so above.
