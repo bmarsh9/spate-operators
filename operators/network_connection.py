ed6e48e63ab636254d3f8caa6c79448a57ff771b
bmarsh9 <bmarshall735@gmail.com>
2021-12-13 21:14:22 -0500

    Update network_connection.py

--- operators/network_connection.py
+++ operators/network_connection.py
@@ -1,7 +1,12 @@
 def code(input, **kwargs):
     '''Place your custom code below.
     Must be indented under this function.'''
-
+    url = locker(kwargs["config"],"default","url_host") ##input:type=text:name=url_host:label=Enter the hostname for the URL connection:placeholder=e.g http://test.com
+    r = request.get(url)
+    if r.ok:
+        return r.json()
+    else:
+        logging.warning("network request to {} failed".format(url))
 
     '''Default return is True. If you want to return something else, do so above.
     If the return is False, the workflow will NOT proceed.'''
