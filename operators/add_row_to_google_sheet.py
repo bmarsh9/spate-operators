40d95f26d7bebaa59ec58215dc0b54be3c659e4c
bmarsh9 <bmarshall735@gmail.com>
2021-12-14 11:40:27 -0500

    Update add_row_to_google_sheet.py

--- operators/add_row_to_google_sheet.py
+++ operators/add_row_to_google_sheet.py
@@ -2,7 +2,12 @@ def code(input, **kwargs):
     '''Place your custom code below.
     Must be indented under this function.'''
 
-
+    # https://github.com/nithinmurali/pygsheets
+    SCOPES = ('https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive')
+    service_account_info = json.loads(secret_dict)
+    my_credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
+    gc = pygsheets.authorize(custom_credentials=my_credentials)
+    
     '''Default return is True. If you want to return something else, do so above.
     If the return is False, the workflow will NOT proceed.'''
     return input
