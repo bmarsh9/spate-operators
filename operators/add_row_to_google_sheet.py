5237fb6904f27f753f44a6c5525aba0e5fd66d28
bmarsh9 <bmarshall735@gmail.com>
2021-12-14 11:51:03 -0500

    Update add_row_to_google_sheet.py

--- operators/add_row_to_google_sheet.py
+++ operators/add_row_to_google_sheet.py
@@ -1,12 +1,16 @@
 def code(input, **kwargs):
     '''Place your custom code below.
     Must be indented under this function.'''
-
+    # https://github.com/nithinmurali/pygsheets
     secret_dict = locker(kwargs["config"],"default","004_secretdict") ##input:type=text:name=004_secretdict:label=Oauth credentials :placeholder=e.g. {}
 
-    # https://github.com/nithinmurali/pygsheets
+    try:
+        service_account_info = json.loads(secret_dict)
+    except:
+        logging.error("faile to load credentials")
+        return False
+    
     SCOPES = ('https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive')
-    service_account_info = json.loads(secret_dict)
     my_credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
     gc = pygsheets.authorize(custom_credentials=my_credentials)
     
