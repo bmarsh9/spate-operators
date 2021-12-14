2141d87bdd753e0fe9c13cf16abf3f4c9ce78c32
bmarsh9 <bmarshall735@gmail.com>
2021-12-13 21:51:53 -0500

    Update network_connection.py

--- operators/network_connection.py
+++ operators/network_connection.py
@@ -3,11 +3,13 @@ def code(input, **kwargs):
     Must be indented under this function.'''
     url = locker(kwargs["config"],"default","url_host") ##input:type=text:name=url_host:label=Enter the hostname for the URL connection:placeholder=Enter the host with http
     verify = locker(kwargs["config"],"default","verify") ##input:type=checkbox:name=verify:label=Verify TLS connection
-    r = requests.get(url,verify=verify)
+    headers = locker(kwargs["config"],"default","headers") or {} ##input:type=text:name=headers:label=Set TLS headers:placeholder=Dictionary of headers for the request    
+    r = requests.get(url,verify=verify,headers=headers)
     if r.ok:
         return r.json()
     else:
         logging.warning("network request to {} failed".format(url))
+        return None
 
     '''Default return is True. If you want to return something else, do so above.
     If the return is False, the workflow will NOT proceed.'''
