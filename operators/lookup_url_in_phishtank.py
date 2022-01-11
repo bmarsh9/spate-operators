fa1f818c9909c3f057812dacdf393b52eef00ccf
bmarsh9 <bmarshall735@gmail.com>
2022-01-10 21:13:52 -0500

    Create lookup_url_in_phishtank.py

---
+++ operators/lookup_url_in_phishtank.py
@@ -0,0 +1,28 @@
+def code(input, **kwargs):
+    '''Place your custom code below.
+    Must be indented under this function.'''
+
+    headers = {
+        'format': 'json',
+        'app_key': '',
+    }
+
+    pt_api_key = locker(kwargs["config"],"default","006_ptapikey") ##input:type=text:name=006_ptapikey:label=Set Phishtank API Key:placeholder=Input API key for Phishtank
+    if not pt_api_key:
+        pt_api_key = ""
+        logging.warning("<pt_api_key> is empty. You may hit a lower rate limit.")
+    headers["app_key"] = pt_api_key
+    phishtank_url = "http://checkurl.phishtank.com/checkurl/"
+    new_check_bytes = URI.encode()
+    base64_bytes = base64.b64encode(new_check_bytes)
+    base64_new_check = base64_bytes.decode('ascii')
+    phishtank_url += base64_new_check
+    response = requests.post(url=phishtank_url, headers=headers)
+    
+    # phishtank API returns data in XML
+    results = ET.ElementTree(ET.fromstring(response.text))
+    return results
+
+    '''Default return is True. If you want to return something else, do so above.
+    If the return is False, the workflow will NOT proceed.'''
+    return input
